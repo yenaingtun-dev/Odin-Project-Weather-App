de28be1494f94575d99a840713598539ae679cbb
Ye Naing Tun <yenaingtun425@gmail.com>
2023-12-04 10:40:49 +0630

    added ui and function

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const form = document.getElementById("form");
+const temperature = document.querySelector(".temp");
+const city = document.querySelector(".city");
+const country_name = document.querySelector(".country_name");
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const country = document.getElementById("country").value;
@@ -10,10 +14,12 @@ async function getWeather(country) {
         `https://api.openweathermap.org/data/2.5/weather?q=${country}&appid=6474b5c914961f6832d52f0255e59890&units=metric`
     );
     const data = await response.json();
-    const feel_like = data.main.feels_like;
     const temp = data.main.temp;
-    const countryName = data.name;
-    console.log(countryName);
-    console.log("temp " + temp);
-    console.log("feel like " + feel_like);
+    const countryName = data.sys.country;
+    const cityName = data.name;
+    temperature.innerHTML = temp;
+    temperature.append("°C");
+    city.textContent = cityName;
+    city.append(",");
+    country_name.innerHTML = countryName;
 }
